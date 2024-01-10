44b966216efc6cba1fee0526aaffc10831790c6d
nhatndq-nals <nhatndq@nal.vn>
2024-01-10 16:23:48 +0700

    Fix Stage Two

--- memories/src/components/stageTwo.tsx
+++ memories/src/components/stageTwo.tsx
@@ -1,24 +1,23 @@
 import React, { memo } from "react";
-import Image from "next/image";
+
+import ImageKit from "@/components/common/customImage";
 
 import styles from "@/styles/stageTwo.module.scss";
 import { _clx } from "@/utils/common";
+import { CONTENT } from "@/utils/const";
 
 const StageTwo = memo(() => {
   return (
     <div className={_clx(styles.wrapper_content)}>
       <div className={styles.content}>
-        {Array(30)
-          .fill(null)
-          .map((_, index) => index)
-          .map((index) => (
+        {CONTENT.page_two
+          .map((image, index) => (
             <div key={index} className={styles.wrapper_image}>
-              <Image
-                src={`/images/sample_${(index % 4) + 1}.jpeg`}
-                alt=''
+              <ImageKit
+                src={image.url}
+                alt={image.label}
                 width={100}
                 height={100}
-                style={{ objectFit: "contain" }}
               />
             </div>
           ))}
--- memories/src/utils/const.ts
+++ memories/src/utils/const.ts
@@ -9,12 +9,280 @@ type Content = {
     welcome_second: string;
     next: string;
   };
+  page_two: {
+    url: string;
+    label: string;
+  }[];
 };
 
 type ContentGroup = {
   DEV: Content;
   PROD: Content;
 };
+
+export const IMAGE_KIT_URL = process.env.NEXT_PUBLIC_IMAGE_KIT ?? "";
+
+export const IMAGE_LIST_PROD = [
+  {
+    url: "/20-09-2020-airport",
+    label: "",
+  },
+  {
+    url: "/30-04-2021-part-1",
+    label: "",
+  },
+  {
+    url: "/30-04-2021-part-2",
+    label: "",
+  },
+  {
+    url: "/30-04-2021-part-3",
+    label: "",
+  },
+  {
+    url: "/30-04-2021-part-4",
+    label: "",
+  },
+  {
+    url: "/02-05-2021-part-1",
+    label: "",
+  },
+  {
+    url: "/02-05-2021-part-2",
+    label: "",
+  },
+  {
+    url: "/02-05-2021-part-3",
+    label: "",
+  },
+  {
+    url: "/02-05-2021-part-4",
+    label: "",
+  },
+  {
+    url: "/02-05-2021-part-5",
+    label: "",
+  },
+  {
+    url: "/19-06-2021-danang",
+    label: "",
+  },
+  {
+    url: "/10-10-2021-hanoi",
+    label: "",
+  },
+  {
+    url: "/07-11-2021-hanriver",
+    label: "",
+  },
+  {
+    url: "/07-11-2021-airport",
+    label: "",
+  },
+  {
+    url: "/15-01-2022-airport",
+    label: "",
+  },
+  {
+    url: "/14-02-2022-valentine",
+    label: "",
+  },
+  {
+    url: "/26-03-2022-hue",
+    label: "",
+  },
+  {
+    url: "/12-05-2022-saigon",
+    label: "",
+  },
+  {
+    url: "/18-03-2023",
+    label: "",
+  },
+  {
+    url: "/19-03-2023-hoian",
+    label: "",
+  },
+  {
+    url: "/30-03-2023",
+    label: "",
+  },
+  {
+    url: "/21-04-2023",
+    label: "",
+  },
+  {
+    url: "/04-05-2023",
+    label: "",
+  },
+  {
+    url: "/07-05-2023-beautiful-day",
+    label: "",
+  },
+  {
+    url: "/27-05-2023",
+    label: "",
+  },
+  {
+    url: "/29-07-2023-mangden-travel",
+    label: "",
+  },
+  {
+    url: "/23-09-2023",
+    label: "",
+  },
+  {
+    url: "/30-09-2023",
+    label: "",
+  },
+  {
+    url: "/23-11-2023",
+    label: "",
+  },
+  {
+    url: "/02-12-2023-prepare-for-exam",
+    label: "",
+  },
+  {
+    url: "/24-12-2023-xmas",
+    label: "",
+  },
+  {
+    url: "/26-04-2023-ngaongo",
+    label: "",
+  },
+];
+
+export const IMAGE_LIST_DEV = [
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+  {
+    url: "/sample_1",
+    label: "",
+  },
+  {
+    url: "/sample_2",
+    label: "",
+  },
+  {
+    url: "/sample_3",
+    label: "",
+  },
+  {
+    url: "/sample_4",
+    label: "",
+  },
+];
 
 const DEV_TEXT = {
   page_login: {
@@ -27,6 +295,7 @@ const DEV_TEXT = {
     welcome_second: "Pellentesque habitant morbi tristique senectus et netus?",
     next: "Cras vehicula in erat eleifend ornare. Nullam.",
   },
+  page_two: IMAGE_LIST_DEV,
 } as Content;
 
 const PROD_TEXT = {
@@ -40,6 +309,7 @@ const PROD_TEXT = {
     welcome_second: "Em đã sẳn sàng khám phá chưa nào?",
     next: "Nhấn bất kỳ để đi đến trang tiếp nhé.",
   },
+  page_two: IMAGE_LIST_PROD,
 } as Content;
 
 export const TEXT_CONTENT = {
