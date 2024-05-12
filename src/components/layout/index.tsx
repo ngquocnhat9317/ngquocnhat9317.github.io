d50765227f044ca2b55838ebcc742038a3709330
nhatndq-nals <48057924+ngquocnhat9317@users.noreply.github.com>
2024-05-12 14:03:41 +0700

    add sidebar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,26 +1,54 @@
 import Head from "next/head";
-import React, { ReactNode, memo } from "react";
+import React, { ReactNode, memo, useState } from "react";
 
 import Header from "@/components/header";
+import styles from "@/styles/Layout.module.scss";
+import { _clsx } from "@/utilities/common";
+
+import SideBar from "../sidebar";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const DefaultLayout = memo(({ children }: LayoutProps) => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
   return (
     <React.Fragment>
       <Head>
-        <meta property="og:url" content="https://ngquocnhat9317.github.io" />
+        <meta property="og:url" content="https://wedding-qn-tt.site" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Save the date - Quốc Nhật & Thuỳ Trang - 26/05/2024" />
         <meta property="og:title" content="Save the date - Quốc Nhật & Thuỳ Trang - 26/05/2024" />
         <meta property="og:description" content="1 chiếc thiệp cưới của Quắc Tô và Thị Ngáo" />
         <meta property="og:image" content="https://ik.imagekit.io/ngquocnhat9317/save_the_date/thumbnail.jpg" />
       </Head>
-      <div className="h-screen w-full relative">
+      <div className="h-screen w-full relative overflow-hidden">
         <Header />
         <main className="w-full h-full md:h-[calc(100%-50px)]">{children}</main>
+        <SideBar toggle={toggle} setToggle={setToggle} />
+        <button
+          type="button"
+          className={_clsx(
+            "absolute bottom-[65px] right-[15px] md:bottom-[15px]",
+            "w-[35px] h-[35px]",
+            "opacity-70 md:opacity-50 md:hover:opacity-70",
+            styles.icon,
+            styles.icon_play,
+          )}
+        />
+        <button
+          type="button"
+          className={_clsx(
+            "absolute bottom-[15px] right-[15px] md:hidden",
+            "w-[35px] h-[35px]",
+            "opacity-70 md:opacity-50 md:hover:opacity-70",
+            styles.icon,
+            styles.icon_burger,
+          )}
+          onClick={() => setToggle(true)}
+        />
       </div>
     </React.Fragment>
   );
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,69 @@
+import { memo, useCallback, useEffect, useState } from "react";
+
+import { CabinText } from "@/fonts";
+import stylesHeader from "@/styles/Header.module.scss";
+import stylesLayout from "@/styles/Layout.module.scss";
+import { _clsx } from "@/utilities/common";
+import { HEADER } from "@/utilities/constant";
+
+type SideBarProps = {
+  toggle: boolean;
+  setToggle: (data: boolean) => void;
+};
+
+const SideBar = memo(({ toggle, setToggle }: SideBarProps) => {
+  const [insideToggle, setInsideToggle] = useState<boolean>(false);
+
+  useEffect(() => {
+    setInsideToggle(toggle);
+  }, [toggle]);
+
+  const handleScroll = useCallback(
+    (id: string) => () => {
+      setToggle(false);
+      const element = document.getElementById(id);
+
+      element?.scrollIntoView({
+        behavior: "smooth",
+      });
+    },
+    [],
+  );
+
+  return (
+    <aside
+      className={_clsx(
+        "absolute md:hidden top-0 w-full h-full",
+        stylesHeader.aside,
+        insideToggle && stylesHeader.aside_show,
+      )}
+    >
+      <div className="w-full h-full flex justify-around flex-col relative">
+        <button
+          type="button"
+          className={_clsx(
+            "absolute top-[15px] left-[15px]",
+            "w-[20px] h-[20px]",
+            "opacity-70 md:opacity-50 md:hover:opacity-70",
+            stylesLayout.icon,
+            stylesLayout.icon_close,
+          )}
+          onClick={() => setToggle(false)}
+        />
+        {Object.values(HEADER).map(({ label, id }) => (
+          <h1
+            key={id}
+            className={_clsx(stylesHeader.text, CabinText.className, "text-[18px] text-center")}
+            onClick={handleScroll(id)}
+          >
+            {label}
+          </h1>
+        ))}
+      </div>
+    </aside>
+  );
+});
+
+SideBar.displayName = "SideBar";
+
+export default SideBar;
